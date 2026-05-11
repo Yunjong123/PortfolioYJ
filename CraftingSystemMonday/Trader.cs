792540a02c6efd155573cbecc933c1ae307390cc
Yunjong123 <heo789987@gmail.com>
2026-05-11 15:57:50 -0500

    Delete CraftingSystemMonday directory

--- CraftingSystemMonday/Trader.cs
+++
@@ -1,9 +0,0 @@
-namespace CraftingSystemMonday
-{
-    public class Trader : Person
-    {
-        public Trader() : base("Vendor", 0.00) { }
-
-        public Trader(string name, double currency) : base(name, currency) { }
-    }
-}
